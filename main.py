cb5b2fe42e00b515a4feaac5ecff69195b4cc979
rob <rowntreerob@gmail.com>
2024-09-12 14:41:33 -0700

    initial commit for fastapi app , railway integration
    Signed-off-by: rob <rowntreerob@gmail.com>

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-'''
-This is a scrip that will be used in the setup.py as an entry point.
-Instantiate all your classes here.
-'''
-import argparse
-import sys
-from main_project_package import project_root
-from main_project_package.SimpleClass import SimpleClass
+from fasthtml.fastapp import *
+from fastapi import FastAPI
+from fastapi import  Request
+#from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="template")
 
-def parse_input_args(argv):
-    ''' Parses command line arguments '''
-
-    parser = argparse.ArgumentParser(description=
-            "Description of the app that will be displayed when the script is executed.")
-    parser.add_argument('--test', help="Test the app.", dest="test",
-                        action='store_true', required=False)
-
-
-def execute_script(input_args):
-    config_file = "{}/config.yaml".format(project_root.path())
-    parsed_args = parse_input_args(input_args)
-
-    if parsed_args.test:
-        print("Testing the app.")
-        classObject = SimpleClass(config_file)
-
-
-def main():
-    # Entry point to the app. Call in test method
-    execute_script(sys.argv[1:])
-
-
-if __name__ == "__main__":
-    main()
+@app.get("/script")
+async def read_item(request: Request, body: str = 'default'):
+    return templates.TemplateResponse(
+        request=request, name="athletic.html", context={"body": body})
